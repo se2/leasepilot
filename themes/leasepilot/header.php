313695e4bd3a5dc3a6bc9dc053a1a701ad3762c1
Tu Luong <tu@delindesign.com>
2018-05-16 23:49:00 -0400

    Use offcanvas menu, update page header mobile

--- themes/leasepilot/header.php
+++ themes/leasepilot/header.php
@@ -18,6 +18,10 @@
 	</head>
 	<body <?php body_class(); ?>>
 
+	<?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) === 'offcanvas' ) : ?>
+		<?php get_template_part( 'template-parts/mobile-off-canvas' ); ?>
+	<?php endif; ?>
+
 	<header class="site-header" role="banner">
 		<div class="site-title-bar title-bar" <?php foundationpress_title_bar_responsive_toggle(); ?>>
 			<div class="title-bar-left">
@@ -26,7 +30,7 @@
 		</div>
 		<div class="grid-container">
 			<div class="grid-x flex-bottom">
-				<div class="cell medium-3 large-3">
+				<div class="cell small-12 medium-3 large-3">
 					<div class="top-bar-left">
 						<div class="site-desktop-title top-bar-title">
 							<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
@@ -35,7 +39,7 @@
 						</div>
 					</div>
 				</div>
-				<div class="cell medium-9 large-9">
+				<div class="cell hide-for-small-only hide-for-medium-only large-9">
 					<div class="top-menu">
 						<?php wp_nav_menu(array(
 							'menu' => 'Top Menu'
