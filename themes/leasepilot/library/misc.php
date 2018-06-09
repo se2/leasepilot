17c1f82b0af91283b873e5e4dc45f94411b36d96
Tu Luong <tu@delindesign.com>
2018-06-08 23:32:34 -0400

    Add Posts Widget with ACF post object repeater

--- themes/leasepilot/library/misc.php
+++ themes/leasepilot/library/misc.php
@@ -78,3 +78,89 @@ function my_mce4_options( $init ) {
 }
 
 add_filter( 'tiny_mce_before_init', 'my_mce4_options' );
+
+
+if ( ! class_exists( 'PostsWidget' ) ) {
+
+	class PostsWidget extends WP_Widget {
+
+		/**
+		* Sets up the widgets name etc
+		*/
+		public function __construct() {
+			$widget_ops = array(
+				'classname'   => 'posts_widget',
+				'description' => 'Posts Widget',
+			);
+			parent::__construct( 'posts_widget', 'Posts Widget', $widget_ops );
+		}
+
+		/**
+		* Outputs the content of the widget
+		*
+		* @param array $args
+		* @param array $instance
+		*/
+		public function widget( $args, $instance ) {
+			// Outputs the content of the widget.
+			if ( ! isset( $args['widget_id'] ) ) {
+				$args['widget_id'] = $this->id;
+			}
+
+			// Widget ID with prefix for use in ACF API functions.
+			$widget_id = 'widget_' . $args['widget_id'];
+			$title     = get_field( 'title', $widget_id ) ? get_field( 'title', $widget_id ) : '';
+			$posts     = get_field( 'posts', $widget_id );
+
+			echo $args['before_widget'];
+
+			if ( $title ) {
+				echo $args['before_title'] . esc_html($title) . $args['after_title'];
+			}
+
+			if ( $posts ) {
+				echo '<ul>';
+				foreach ( $posts as $key => $post ) {
+					$post_object = $post['post'];
+					echo '<li><a href="' . get_permalink( $post_object->ID ) . '">' . $post_object->post_title . '</a></li>';
+				}
+				echo '</ul>';
+			}
+
+			echo $args['after_widget'];
+
+		}
+
+		/**
+		 * Outputs the options form on admin
+		 *
+		 * @param array $instance The widget options
+		 */
+		public function form( $instance ) {
+			// outputs the options form on admin
+		}
+
+		/**
+		 * Processing widget options on save
+		 *
+		 * @param array $new_instance The new options
+		 * @param array $old_instance The previous options
+		 *
+		 * @return array
+		 */
+		public function update( $new_instance, $old_instance ) {
+			// processes widget options to be saved
+		}
+
+	}
+
+}
+
+/**
+ * Register our CTA Widget
+ */
+function register_posts_widget() {
+	register_widget( 'PostsWidget' );
+}
+
+add_action( 'widgets_init', 'register_posts_widget' );
