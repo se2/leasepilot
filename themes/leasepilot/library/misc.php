02eb0ceeab32e49bcc599b0f6619e1f03e0a9a46
Tu Luong <tu@delindesign.com>
2018-06-08 23:48:26 -0400

    Add standard OpenGraph tags for posts

--- themes/leasepilot/library/misc.php
+++ themes/leasepilot/library/misc.php
@@ -164,3 +164,38 @@ function register_posts_widget() {
 }
 
 add_action( 'widgets_init', 'register_posts_widget' );
+
+/**
+ * Standard OpenGraph
+ */
+function fb_opengraph() {
+	global $post;
+
+	if ( is_single() ) {
+		if ( has_post_thumbnail( $post->ID ) ) {
+			$img_src = get_the_post_thumbnail_url( $post->ID, 'medium' );
+		} else {
+			$img_src = get_stylesheet_directory_uri() . '/src/assets/leasepilot-logo.jpg';
+		}
+		if ( $excerpt = $post->post_excerpt ) {
+				$excerpt = strip_tags( $post->post_excerpt );
+				$excerpt = str_replace( "", "'", $excerpt );
+		} else {
+				$excerpt = get_bloginfo( 'description' );
+		}
+		?>
+
+	<meta property="og:title" content="<?php echo the_title(); ?>"/>
+	<meta property="og:description" content="<?php echo $excerpt; ?>"/>
+	<meta property="og:type" content="article"/>
+	<meta property="og:url" content="<?php echo the_permalink(); ?>"/>
+	<meta property="og:site_name" content="<?php echo get_bloginfo(); ?>"/>
+	<meta property="og:image" content="<?php echo $img_src; ?>"/>
+
+<?php
+	} else {
+			return;
+	}
+}
+
+add_action( 'wp_head', 'fb_opengraph', 5 );
