9cf40f5998eef92295709c7d9d30612872e37e57
Tu Luong <tu@delindesign.com>
2018-06-13 12:05:20 -0400

    Update footer address, socials and style

--- themes/leasepilot/footer.php
+++ themes/leasepilot/footer.php
@@ -10,35 +10,38 @@
 ?>
 
 <footer class="footer">
-    <div class="footer-container">
-        <div id="footer-widgets" class="grid-x grid-margin-x">
-            <?php for ($i = 1; $i <= 6 ; $i++): ?>
-            <div id="footer-widget-<?php echo $i; ?>" class="footer-menu small-12 medium-3 large-2 cell">
-                <?php if ( is_active_sidebar( 'footer-widget-' . $i ) ) { dynamic_sidebar( 'footer-widget-' . $i ); } ?>
-            </div>
-            <?php endfor; ?>
-        </div>
-    </div>
-    <div class="footer-container">
-        <div class="grid-x">
-            <div class="cell medium-3">
-                <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-                    <img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/leasepilot-logo-footer.png" alt="<?php bloginfo(); ?>" class="site-logo">
-                </a>
-            </div>
-            <div class="cell medium-offset-1 medium-5 copyright">
-                <p>© Gadfly © 2018. All rights reserved.</p>
-            </div>
-            <div class="cell medium-3 socials">
-                <a href="#" class="social-link">
-                    <span class="icon-twitter"></span>
-                </a>
-                <a href="#" class="social-link">
-                    <span class="icon-linkedin"></span>
-                </a>
-            </div>
-        </div>
-    </div>
+	<div class="footer-container">
+		<div id="footer-widgets" class="grid-x grid-margin-x">
+			<?php for ($i = 1; $i <= 6 ; $i++): ?>
+			<div id="footer-widget-<?php echo $i; ?>" class="footer-menu small-12 medium-3 large-2 cell">
+					<?php if ( is_active_sidebar( 'footer-widget-' . $i ) ) { dynamic_sidebar( 'footer-widget-' . $i ); } ?>
+			</div>
+			<?php endfor; ?>
+		</div>
+	</div>
+	<div class="footer-container">
+		<div class="grid-x">
+			<div class="cell medium-12 large-3">
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+					<img src="<?php echo get_template_directory_uri(); ?>/dist/assets/images/leasepilot-logo-footer.png" alt="<?php bloginfo(); ?>" class="site-logo">
+				</a>
+			</div>
+			<div class="cell medium-5 large-4 copyright">
+				<p>800 Boyleston St. 29th Floor 02199</p>
+			</div>
+			<div class="cell medium-5 large-4 copyright">
+				<p>© Gadfly © 2018. All rights reserved.</p>
+			</div>
+			<div class="cell medium-2 large-1 socials">
+				<a href="https://twitter.com/theleasepilot" class="social-link" target="_blank">
+					<span class="icon-twitter"></span>
+				</a>
+				<a href="https://www.linkedin.com/company/leasepilot" class="social-link" target="_blank">
+					<span class="icon-linkedin"></span>
+				</a>
+			</div>
+		</div>
+	</div>
 </footer>
 
 <?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) === 'offcanvas' ) : ?>
@@ -46,5 +49,7 @@
 <?php endif; ?>
 
 <?php wp_footer(); ?>
+
 </body>
+
 </html>
