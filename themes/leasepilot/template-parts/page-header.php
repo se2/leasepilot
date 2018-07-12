9855b6d39b2ec8b9f6ef2600b7d172a42f9ca535
Tu Luong <tu@delindesign.com>
2018-07-12 13:09:00 -0400

    Use YouTube API

--- themes/leasepilot/template-parts/page-header.php
+++ themes/leasepilot/template-parts/page-header.php
@@ -93,15 +93,59 @@ if ( get_field( $prefix . 'background_position', $option ) ) {
 		if ( 'vimeo' === $video_type ) :
 	?>
 	<div class="vimeo-wrapper show-for-large">
-		<iframe src="https://player.vimeo.com/video/<?php echo esc_attr( $vimeo_id ); ?>?background=1&autoplay=1&loop=1&title=0&byline=0&muted=1" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+		<!-- &loop=1 -->
+		<iframe src="https://player.vimeo.com/video/<?php echo esc_attr( $vimeo_id ); ?>?background=1&autoplay=1&title=0&byline=0&muted=1&sidedock=0" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 	</div>
 	<?php
 		elseif ( 'youtube' === $video_type ) :
 	?>
 	<div class="yt-bg hide-for-xlarge" style="<?php echo esc_attr( $bg ); ?>;"></div>
 	<div class="vimeo-wrapper show-for-xlarge">
-		<iframe src="https://www.youtube.com/embed/<?php echo esc_attr( $youtube_id ); ?>?autoplay=1&controls=0&loop=1&playlist=<?php echo esc_attr( $youtube_id ); ?>" allow="autoplay; encrypted-media" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+		<!-- &loop=1&playlist=<?php // echo esc_attr( $youtube_id ); ?> -->
+		<iframe id="ytplayer" src="https://www.youtube.com/embed/<?php echo esc_attr( $youtube_id ); ?>?modestbranding=1&enablejsapi=1&autoplay=1&controls=0&rel=0" allow="autoplay; encrypted-media" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 	</div>
+	<script>
+		// Load the IFrame Player API code asynchronously.
+		var tag = document.createElement('script');
+		tag.src = "https://www.youtube.com/player_api";
+		var firstScriptTag = document.getElementsByTagName('script')[0];
+		firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+		// Replace the 'ytplayer' element with an <iframe> and
+		// YouTube player after the API code downloads.
+		var player;
+		function onYouTubePlayerAPIReady() {
+			player = new YT.Player('ytplayer', {
+				events: {
+          'onReady': onPlayerReady,
+          'onStateChange': onPlayerStateChange
+        }
+			});
+		}
+		// The API will call this function when the video player is ready.
+		function onPlayerReady(event) {
+			// event.target.playVideo();
+		}
+
+		// The API calls this function when the player's state changes.
+		// YT.PlayerState.UNSTARTED (-1)
+		// YT.PlayerState.ENDED (0)
+		// YT.PlayerState.PLAYING (1)
+		// YT.PlayerState.PAUSED (2)
+		// YT.PlayerState.BUFFERING (3)
+		// YT.PlayerState.CUED (5)
+		var done = false;
+		function onPlayerStateChange(event) {
+			if (event.data == YT.PlayerState.PLAYING && !done) {
+				setTimeout(pauseVideo, (player.getDuration() * 1000 - 100));
+				done = true;
+			}
+		}
+		function pauseVideo() {
+			player.pauseVideo();
+		}
+	</script>
+
 	<?php
 		else :
 	?>
